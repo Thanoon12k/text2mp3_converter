3e27bc7fa36a0d6f6d40e2b279e810f322554307
Thanoon Younes <engthanoon1@gmail.com>
2023-03-11 23:23:57 +0300

    add arabic language and fix issuse image deleting

--- home/converter.py
+++ home/converter.py
@@ -1,11 +1,11 @@
 
 from gtts import gTTS 
 import os 
-
+from base.settings import BASE_DIR
 def to_mp3(text,name):
-	language = 'en'
+	language = 'ar'
 	myobj = gTTS(text=text, lang=language, slow=False)
-	myobj.save("media/"+name)
+	myobj.save(str(BASE_DIR)+"media/"+name)
 	# os.system("start media/output.mp3")
 	return name
 
--- home/views.py
+++ home/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from .converter import to_mp3
 import os
 import random
+from base.settings import BASE_DIR
+
 def Home(request):
     if request.method=='POST':
         clean()
@@ -12,7 +14,7 @@ def Home(request):
             name=f'output_{random.randint(0, 5000)}.mp3'
             name=to_mp3(txt,name)
         
-            file_path = 'media/'+name
+            file_path =str(BASE_DIR)+ 'media/'+name
             with open(file_path, 'rb') as pdf_file:
                 response = HttpResponse(pdf_file.read(), content_type='audio/mp3')
                 response[f'Content-Disposition'] = f'attachment; filename="{name}"'
@@ -21,11 +23,12 @@ def Home(request):
     return render(request,'home.html')
 
 def clean():
-    folder_path='media/'
+    
+    folder_path=str(BASE_DIR)+'/media/'
     for filename in os.listdir(folder_path):
         file_path = os.path.join(folder_path, filename)
         try:
-            if os.path.isfile(file_path) and 'mp3' in file_path:
+            if os.path.isfile(file_path) and '.mp3' in file_path:
                 os.remove(file_path)
         except Exception as e:
             print(f"Error deleting {file_path} : {e}")
